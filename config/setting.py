13968bd5c375bb73051ede00c923e12a0e378b22
房文同 <fangwentong2012@gmail.com>
2014-10-06 12:18:14 +0800

    updada

--- config/setting.py
+++ config/setting.py
@@ -3,14 +3,26 @@
 
 import web
 import os
+import time
 
 render = web.template.render('templates/', cache=False)
 
 web.config.debug = True
 
 site = web.storage (
-        title = U"哈工大饮食中心",
-        brand = U"哈工大饮食中心",
+        title  = U"哈工大饮食中心",
+        brand  = U"哈工大饮食中心",
+
+        date   = web.storage (
+            year  = time.strftime('%Y',time.localtime(time.time())),
+            month = time.strftime('%m',time.localtime(time.time())),
+            day   = time.strftime('%d',time.localtime(time.time())),
+            ),
+
+        owner = web.storage (
+            name = U"哈尔滨工业大学 饮食中心",
+            site = "#",
+            ),
 
         author = web.storage (
             name     = "fangwentong",
@@ -28,10 +40,10 @@ login = web.storage (
 
 if 'SERVER_SOFTWARE' in os.environ:
     asset_path = "/static"
-    image_url = "hitfoodcenter.qiniudn.com"
+    image_url = "hitfoodcenter.qiniudn.com/image"
 else:
     asset_path = "/static"
-    image_url = ""
+    image_url = "/image"
 
 web.template.Template.globals['site'] = site
 web.template.Template.globals['render'] = render
